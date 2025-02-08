47bb9c850ee47a7a28cdb50f6228deea40c95d07
Your Name <you@example.com>
2025-02-08 13:46:11 +0500

    initial commit

--- src/sanity/schemaTypes/index.ts
+++ src/sanity/schemaTypes/index.ts
@@ -1,8 +1,7 @@
 import { type SchemaTypeDefinition } from 'sanity';
 
-import foods from './Foods';
 import chefs from './Chefs';
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [foods, chefs],
+  types: [ chefs],
 };
